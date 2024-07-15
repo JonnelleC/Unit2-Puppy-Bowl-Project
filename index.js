7b60bc256e057509fb65eda7ae790df6bc2e52d3
JonnelleC <carrjonnelle@gmail.com>
2024-07-15 13:26:44 -0400

    players pictures on the page

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const getPlayers = async () => {
     try {
-      const response = fetch(
+      const response = await fetch(
         `https://fsa-puppy-bowl.herokuapp.com/api/2406-FTB-ET-WEB-FT/players`
       );
       if (!response.ok) {
         throw new Error(`HTTP Error!`);
       }
       const responseJson = await response.json();
-      const playerData = responseJson.players;
+      const playerData = responseJson.data.players;
       console.log(playerData);
 
       //   const playerImage =  playerData.imageUrl.front_default;
@@ -19,11 +19,14 @@ document.addEventListener("DOMContentLoaded", async () => {
       playerData.forEach((player) => {
         const playerElement = document.createElement("li");
         playerElement.innerHTML = `
-        <li>${player.name}</li>
-       <li>${player.breed} </li>
-       <li>${player.status}</li>
-       <li><img src="${player.imageUrl}"></li>`;
+        <div>
+        <p>${player.name}</p>
+       <p>${player.breed} </p>
+       <p>${player.status}</p>
+       <img src="${player.imageUrl}"></div>`;
+       playerContainer.appendChild(playerElement);
       });
+      
     } catch (error) {
       console.error("Could not fetch resource", error);
     }
